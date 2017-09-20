152ac8a395d16dc984ef0b7341f29728ccd6d4b4
Vince Bloise <vbloise3@verizon.net>
2017-09-19 22:25:15 -0400

    Added csv file load

--- dist/tableDataREST.js
+++ dist/tableDataREST.js
@@ -14,6 +14,18 @@ var __param = (this && this.__param) || function (paramIndex, decorator) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const typescript_rest_1 = require("typescript-rest");
+// Load config
+require('dotenv').config();
+/*if (!process.env.PROJECT_ID || !process.env.CLIENT_EMAIL || !process.env.PRIVATE_KEY || !process.env.FIREBASE_DB) {
+    console.error('Missing environment variables!');
+    process.exit(1);
+}*/
+var fs = require('fs');
+var parse = require('csv-parse/lib/sync');
+var path = require('path');
+var cors = require('cors');
+var firebase = require('firebase-admin');
+var elements = [];
 let HelloService = class HelloService {
     sayHello(name) {
         return "Hello " + name;
@@ -52,7 +64,7 @@ let TableDataService = class TableDataService {
             "{position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'}," +
             "{position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'}," +
             "{position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'}," +
-            "{position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'}" +
+            "{position: 20, name: 'Calcium', weight: 40.078, symbol: 'CaAssHole'}" +
             "]";
     }
 };
@@ -67,8 +79,58 @@ __decorate([
 TableDataService = __decorate([
     typescript_rest_1.Path("/tableData")
 ], TableDataService);
+let ElementDataService = class ElementDataService {
+    getElementsData(symbols) {
+        if (symbols) {
+            var delimitedSymbols = symbols.split(',');
+            delimitedSymbols.map(function (symbol) {
+                return symbol.toUpperCase();
+            });
+        }
+        loadSymbols();
+        return elements;
+    }
+};
+__decorate([
+    typescript_rest_1.Path(":symbols"),
+    typescript_rest_1.GET,
+    __param(0, typescript_rest_1.PathParam('symbols')),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", Array)
+], ElementDataService.prototype, "getElementsData", null);
+ElementDataService = __decorate([
+    typescript_rest_1.Path("/elementData")
+], ElementDataService);
 let app = express();
 typescript_rest_1.Server.buildServices(app);
+app.use(cors());
+app.set('port', (process.env.PORT || 3000));
+app.set('projectId', (process.env.PROJECT_ID || ''));
+app.set('clientEmail', (process.env.CLIENT_EMAIL || ''));
+app.set('privateKey', (process.env.PRIVATE_KEY || ''));
+app.set('firebaseDB', (process.env.FIREBASE_DB || ''));
+function getRandomInt(min, max) {
+    return (Math.floor(Math.random() * (max - min + 1)) + min);
+}
+function loadSymbols() {
+    let csv = fs.readFileSync('./companies.csv', 'utf8');
+    elements = parse(csv, { columns: true }).map(element => {
+        let current = getRandomInt(5100, 80000) / 100;
+        let change = getRandomInt(-1000, 1000) / 100;
+        return {
+            name: element.Name,
+            position: element.Position,
+            weight: element.Weight,
+            symbol: element.Symbol
+        };
+    });
+    console.log(elements.length + ' elements loaded at ' + new Date());
+    //ref.set(elements);
+    setTimeout(loadSymbols, 1000 * 60 * 60 * 24); // Reload once a day
+}
+// Load the initial data
+loadSymbols();
 app.listen(3000, function () {
     console.log('Rest Server listening on port 3000!');
 });
--- src/tableDataREST.ts
+++ src/tableDataREST.ts
@@ -1,6 +1,44 @@
 import * as express from "express";
 import {Server, Path, GET, PathParam} from "typescript-rest";
 
+// Load config
+require('dotenv').config();
+
+/*if (!process.env.PROJECT_ID || !process.env.CLIENT_EMAIL || !process.env.PRIVATE_KEY || !process.env.FIREBASE_DB) {
+    console.error('Missing environment variables!');
+    process.exit(1);
+}*/
+
+var fs = require('fs');
+var parse = require('csv-parse/lib/sync');
+var path = require('path');
+var cors = require('cors');
+var firebase = require('firebase-admin');
+var elements = [];
+
+/*
+// Initialize Firebase
+firebase.initializeApp({
+    credential: firebase.credential.cert({
+        projectId: app.get('projectId'),
+        clientEmail: app.get('clientEmail'),
+        privateKey: app.get('privateKey').replace(/\\n/g, '\n')
+    }),
+    databaseURL: app.get('firebaseDB')
+});
+
+var db = firebase.database();
+var ref = db.ref('stocks');
+// end initialize firebase db
+*/
+
+export interface Element {
+    name: string;
+    position: number;
+    weight: number;
+    symbol: string;
+}
+
 @Path("/hello")
 class HelloService {
     @Path(":name")
@@ -34,13 +72,62 @@ class TableDataService {
             "{position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'}," +
             "{position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'}," +
             "{position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'}," +
-            "{position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'}" +
+            "{position: 20, name: 'Calcium', weight: 40.078, symbol: 'CaAssHole'}" +
         "]";
+    }
+}
+
+@Path("/elementData")
+    class ElementDataService {
+    @Path(":symbols")
+    @GET
+    getElementsData( @PathParam('symbols') symbols: string): Array<Element> {
+        if (symbols ) {
+            var delimitedSymbols = symbols.split(',');
+            delimitedSymbols.map(function(symbol) {
+                return symbol.toUpperCase();
+            });
+        }
+        loadSymbols();
+        return elements;
     }
 }
 
 let app: express.Application = express();
 Server.buildServices(app);
+
+app.use(cors());
+app.set('port', (process.env.PORT || 3000));
+app.set('projectId', (process.env.PROJECT_ID || ''));
+app.set('clientEmail', (process.env.CLIENT_EMAIL || ''));
+app.set('privateKey', (process.env.PRIVATE_KEY || ''));
+app.set('firebaseDB', (process.env.FIREBASE_DB || ''));
+
+function getRandomInt(min, max) {
+    return (Math.floor(Math.random() * (max - min + 1)) + min);
+}
+
+function loadSymbols() {
+    let csv = fs.readFileSync('./companies.csv', 'utf8');
+
+    elements = parse(csv, {columns: true}).map(element => {
+        let current = getRandomInt(5100, 80000) / 100;
+        let change = getRandomInt(-1000, 1000) / 100;
+        return {
+            name: element.Name,
+            position: element.Position,
+            weight: element.Weight,
+            symbol: element.Symbol
+        };
+    });
+    console.log(elements.length + ' elements loaded at ' + new Date());
+    //ref.set(elements);
+
+    setTimeout(loadSymbols, 1000 * 60 * 60 * 24); // Reload once a day
+}
+
+// Load the initial data
+loadSymbols();
 
 app.listen(3000, function() {
     console.log('Rest Server listening on port 3000!');
